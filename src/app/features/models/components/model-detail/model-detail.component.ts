37d7f5fc48ca21a0563b96c93ba42e83518eb53e
Yunus Emre Civan <emrecivan06x@gmail.com>
2024-05-17 00:57:08 +0300

    update model

--- src/app/features/models/components/model-detail/model-detail.component.ts
+++ src/app/features/models/components/model-detail/model-detail.component.ts
@@ -6,6 +6,7 @@ import {
   GetAllCarResponse,
   GetAllModelResponse,
   GetAllTransmissionResponse,
+  ModelControllerService,
 } from '../../../../shared/services/api';
 import { RouterModule } from '@angular/router';
 
@@ -20,25 +21,35 @@ export class ModelDetailComponent
   extends ModelsListBaseComponent
   implements OnInit
 {
+  modelId!: number;
   @Input() model: GetAllModelResponse | null = null;
-  @Input() car: GetAllCarResponse | null = null;
   @Input() brand: GetAllBrandResponse | null = null;
   @Input() transmission: GetAllTransmissionResponse | null = null;
+  @Input() car: GetAllCarResponse | null = null;
 
   brands: GetAllBrandResponse[] = [];
   transmissions: GetAllTransmissionResponse[] = [];
 
+  constructor(
+    modelsService: ModelControllerService,
+    change: ChangeDetectorRef
+  ) {
+    super(modelsService, change); // Call base class constructor with required services
+  }
+
   override ngOnInit(): void {
-    if (this.model) {
-      this.getModelCardDetail(this.model);
-    }
+    console.log('Received model:', this.model);
+    console.log('Received brand:', this.brand);
+    console.log('Received transmission:', this.transmission);
+    console.log('Received car:', this.car);
+
+    this.change.markForCheck();
   }
 
   getModelCardDetail(model: GetAllModelResponse) {
     console.log('getModelCardDetail is called with model:', model);
 
     const foundBrand = this.brands?.find((brand) => brand.id === model.brandId);
-    debugger;
     console.log('Found brand:', foundBrand);
 
     const foundTransmission = this.transmissions?.find(
@@ -46,7 +57,6 @@ export class ModelDetailComponent
     );
     console.log('Found transmission:', foundTransmission);
 
-    // Eğer bulunan marka ve şanzıman varsa, state'lere atayabiliriz
     if (foundBrand) {
       this.brand = foundBrand;
       console.log('Brand is assigned:', this.brand);
@@ -56,5 +66,6 @@ export class ModelDetailComponent
       this.transmission = foundTransmission;
       console.log('Transmission is assigned:', this.transmission);
     }
+    this.change.markForCheck();
   }
 }
--- src/app/routes/management-page/management-models-page/management-model-detail-page/management-model-detail-page.component.ts
+++ src/app/routes/management-page/management-models-page/management-model-detail-page/management-model-detail-page.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit } from '@angular/core';
 import { ModelDetailComponent } from '../../../../features/models/components/model-detail/model-detail.component';
 import { ActivatedRoute } from '@angular/router';
 import {
+  BrandControllerService,
+  CarControllerService,
   GetAllBrandResponse,
+  GetAllCarResponse,
   GetAllModelResponse,
   GetAllTransmissionResponse,
+  GetBrandByIdRequestParams,
+  GetModelByIdRequestParams,
+  GetTransmissionByIdRequestParams,
+  ModelControllerService,
+  TransmissionControllerService,
 } from '../../../../shared/services/api';
 
 @Component({
@@ -13,15 +21,22 @@ import {
   standalone: true,
   imports: [CommonModule, ModelDetailComponent],
   templateUrl: './management-model-detail-page.component.html',
-  styleUrl: './management-model-detail-page.component.scss',
+  styleUrls: ['./management-model-detail-page.component.scss'],
 })
 export class ManagementModelDetailPageComponent implements OnInit {
   modelId!: number;
   model: GetAllModelResponse | null = null;
   brand: GetAllBrandResponse | null = null;
   transmission: GetAllTransmissionResponse | null = null;
+  car: GetAllCarResponse | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private modelsService: ModelControllerService,
+    private brandsService: BrandControllerService,
+    private transmissionsService: TransmissionControllerService,
+    private carsService: CarControllerService
+  ) {}
 
   ngOnInit(): void {
     this.getModelIdFromRoute();
@@ -33,6 +48,64 @@ export class ManagementModelDetailPageComponent implements OnInit {
       if (!modelId) return;
 
       this.modelId = Number(modelId);
+      console.log('modelId:', this.modelId);
+
+      this.fetchModelDetails(this.modelId);
+    });
+  }
+
+  private fetchModelDetails(modelId: number) {
+    const modelParams: GetModelByIdRequestParams = {
+      id: modelId,
+    };
+    this.modelsService.getModelById(modelParams).subscribe((model) => {
+      this.model = model;
+      console.log('Fetched model:', this.model);
+
+      if (model.brandId !== undefined) {
+        this.fetchBrandDetails(model.brandId);
+        console.log('Brand Id:', model.brandId);
+      }
+
+      if (model.transmissionId !== undefined) {
+        this.fetchTransmissionDetails(model.transmissionId);
+        console.log('Transmission Id:', model.transmissionId);
+      }
+
+      if (model.id !== undefined) {
+        this.fetchCarDetails(model.id);
+        console.log('Car Id:', model.id);
+      }
+    });
+  }
+
+  private fetchBrandDetails(brandId: number) {
+    const brandParams: GetBrandByIdRequestParams = {
+      id: brandId,
+    };
+    this.brandsService.getBrandById(brandParams).subscribe((brand) => {
+      this.brand = brand;
+      console.log('Fetched brand:', this.brand);
+    });
+  }
+
+  private fetchTransmissionDetails(transmissionId: number) {
+    const transmissionParams: GetTransmissionByIdRequestParams = {
+      id: transmissionId,
+    };
+    this.transmissionsService
+      .getTransmissionById(transmissionParams)
+      .subscribe((transmission) => {
+        this.transmission = transmission;
+        console.log('Fetched transmission:', this.transmission);
+      });
+  }
+
+  private fetchCarDetails(modelId: number) {
+    this.carsService.getAllCars().subscribe((cars) => {
+      const car = cars.find((c) => c.modelId === modelId);
+      this.car = car || null;
+      console.log('Fetched car:', this.car);
     });
   }
 }
