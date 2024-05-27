8f3c8b4725d1eb778d647ad60c4f256e6b6d7461
Yunus Emre Civan <emrecivan06x@gmail.com>
2024-05-28 00:03:58 +0300

    update forms

--- src/app/routes/not-found-page/not-found-page.component.ts
+++ src/app/routes/not-found-page/not-found-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from '../../shared/layouts/home-layout/home-layout.component';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HomeLayoutComponent],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
